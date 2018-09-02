3b84f622a6e5579a1d67673742c8b055252d1d3e
hyukhwankwon <khh2302@hotmail.com>
2018-09-02 15:15:24 +0900

    first commir

---
+++ nuxt_with_express/server/app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var { Nuxt, Builder } = require('nuxt')
+
+var config = require('../nuxt.config.js')
+
+const app = express()
+const nuxt = new Nuxt(config)
+
+const builder = new Builder(nuxt)
+builder.build()
+
+app.use(nuxt.render)
+
+var port = 3000
+
+app.listen(port, () => {
+    console.log('Server Lisstening on ' + ':' + port)
+})
